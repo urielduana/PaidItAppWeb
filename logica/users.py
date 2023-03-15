f07844fcc2a8b5455e095141ddf0a2e1624f3ef1
urielduana <81336230+urielduana@users.noreply.github.com>
2023-03-15 11:31:01 -0600

    Ya funciona la conexión de esta madre

--- logica/users.py
+++ logica/users.py
@@ -1,5 +1,5 @@
 # from logica.modelos.usuarios import Users as u
-from models import usuarios as u
+from logica.models import usuarios as u
 
 def create_user(nick_name, full_name):
     user = u.User(nick_name=nick_name, full_name=full_name)
